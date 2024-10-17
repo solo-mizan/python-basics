46aa7726545cfa335d0ad5a0ad4117e043b9d3e7
solo-mizan <mizanbd.eee@gmail.com>
2024-10-17 20:49:34 +0600

    madlibs word game

---
+++ madlibs-game.py
@@ -0,0 +1,16 @@
+# Madlibs Game!
+# Word game where you create a story
+# by filling blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+animal = input("Enter an animal name: ")
+vehicle = input("Enter a vehicle name: ")
+random_object = input("Enter a random object name: ")
+name = input("Enter a man/woman name: ")
+food = input("Enter a food item name: ")
+sound_effect = "moaning"
+plural_noun = "ladies"
+clothing_item = input("Enter a cloth name: ")
+
+
+print(f"\n\nOne day, {name} woke up feeling {adjective1}. He put on his favorite {clothing_item} and went outside. \nSuddenly, {name} saw a(n) {animal} riding a {vehicle} and juggling {plural_noun}! \n'Oh no!' {name} shouted. This is way too {adjective1}! \nJust then, a giant {food} fell from the sky, landing with a loud {sound_effect}. \nPanicking, {name} grabbed a {random_object} and chased after the {animal}. \nIn the end, {name} saved the day by using his {adjective1} {random_object}, and everyone celebrated by dancing and eating {food}. \n\nThe end!")
