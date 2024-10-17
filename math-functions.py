47d49b4e53357774834658509b59e217453c8460
solo-mizan <mizanbd.eee@gmail.com>
2024-10-17 21:20:08 +0600

    math functions

---
+++ math-functions.py
@@ -0,0 +1,26 @@
+import math
+
+x = 2
+y = -6
+z = 9.6
+
+# Absolute value
+abs(x) # 6
+
+# Power fn
+pow(x, 3) # 8
+
+# Maximum fn
+max(x, y, z) # 9.6
+
+# Minimum fn
+min(x, y, z) # -6
+
+# Round fn
+round(z) # 10
+
+# Useful math fn
+math.ceil(z)
+math.floor(z)
+math.sqrt(y)
+math.log(x)
